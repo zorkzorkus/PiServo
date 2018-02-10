9e0ff17ec9ba8622e7972daf112e4493b35f57a1
zork <zorkzorkus@gmail.com>
2018-02-10 21:36:20 +0100

    first working version

--- PiServo.cpp
+++ PiServo.cpp
@@ -1,2 +1,146 @@
+#include <iostream>
+#include <chrono>
+
 #include "PiServo.hpp"
 
+using namespace std;
+using namespace std::chrono_literals;
+
+FLOAT sleepTimeMS = 10.f;
+INT32 Pos[2] = { 0, 0 };
+
+INT32 GetIntInput() {
+	try {
+		string in;
+		getline(cin, in);
+		INT32 res = stoi(in);
+		return res;
+	} catch (exception& e) {
+		cout << e.what() << endl;
+		return GetIntInput();
+	}
+}
+
+FLOAT GetFloatInput() {
+	try {
+		string in;
+		getline(cin, in);
+		FLOAT res = stof(in);
+		return res;
+	} catch (exception& e) {
+		cout << e.what() << endl;
+		return GetFloatInput();
+	}
+}
+
+void Step(Motor m, Direction d) {
+	if (d == Left) {
+		Pi::lStep(m);
+		--Pos[m];
+	} else {
+		Pi::rStep(m);
+		++Pos[m];
+	}
+}
+
+void Move(INT32 x, INT32 y) {
+
+	Direction hor = x > 0 ? Right : Left;
+	Direction vert = y > 0 ? Right : Left;
+
+	x = abs(x);
+	y = abs(y);
+
+	INT32 tx = x;
+	INT32 ty = y;
+
+	INT32 max = max(x, y);
+
+	for (INT32 i = max - 1; i >= 0; --i) {
+
+		FLOAT m = fdiv(i, max);
+		FLOAT fx = fdiv(x, tx);
+		FLOAT fy = fdiv(y, ty);
+
+		if (fx >= m && x > 0) {
+			Step(Horiz, hor);
+			--x;
+		}
+		if (fy >= m && y > 0) {
+			Step(Vertic, vert);
+			--y;
+		}
+
+		sleep(sleepTimeMS);
+
+	}
+
+}
+
+void SetSleepTime() {
+	cout << "Current SleepTime: " << sleepTimeMS << "ms\n > ";
+	FLOAT input = GetFloatInput();
+	if (input < 0) return;
+	sleepTimeMS = input;
+}
+
+void MoveStep() {
+	cout << "Steps Horizontal: ";
+	int x = GetIntInput();
+	cout << "Steps Vertical: ";
+	int y = GetIntInput();
+	Move(x, y);
+}
+
+//using millimeters
+void MoveMilli() {
+	cout << "Move Horizontal: ";
+	FLOAT x = GetFloatInput();
+	cout << "Move Vertical: ";
+	FLOAT y = GetFloatInput();
+	Move(INT32(48 * x), INT32(48 * y));
+}
+
+void Reset() {
+	Move(-Pos[X], -Pos[Y]);
+}
+
+INT32 GetProgramInput() {
+	ClearConsole();
+	cout << "--------------------\n";
+	cout << " Current Position: " << Pos[X] << " | " << Pos[Y] << endl;
+	cout << " 0: Set Sleeptime" << endl;
+	cout << " 1: Move Steps" << endl;
+	cout << " 2: Move Millimeter" << endl;
+	cout << " 5: Reset" << endl;
+	cout << "--------------------\n > ";
+	return GetIntInput();
+}
+
+
+int main() {
+
+	Pi::Init();
+
+	while (true) {
+
+		INT32 input = GetProgramInput();
+
+		switch (input) {
+		case 0:
+			SetSleepTime();
+			break;
+		case 1:
+			MoveStep();
+			break;
+		case 2:
+			MoveMilli();
+			break;
+		case 5:
+			Reset();
+			break;
+		}
+
+	}
+
+}
--- PiServo.hpp
+++ PiServo.hpp
@@ -1,84 +1,127 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
-#define SPEED 10ms
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono_literals;
 
 const float PI = 3.1415926f;
 const int PIN0 = 0x6;
 const int servos = 2;
 
+using UINT32 = unsigned int;
+using INT32 = int;
+using FLOAT = float;
+
 int stepst[servos] = { 0, 0 }; //step state
 
-inline void init_servos() {
-	wiringPiI2CWriteReg8(3, 0x1, 0x4);
-	wiringPiI2CWriteReg8(3, 0x0, 0x1);
+#ifdef max 
+#undef max
+#endif
+
+#ifdef min
+#undef min
+#endif
+
+#define max(a,b) (a > b) ? a : b
+#define min(a,b) (a < b) ? a : b
+
+void inline sleep(FLOAT ms) {
+	this_thread::sleep_for(chrono::duration<float, milli>(ms));
 }
 
-inline void setpwm(int ch, int up, int down) {
-	int choff = 4 * ch;
-	wiringPiI2CWriteReg8(3, PIN0 + choff + 0, up & 0xFF);
-	wiringPiI2CWriteReg8(3, PIN0 + choff + 1, (up >> 8) & 0xF);
-	wiringPiI2CWriteReg8(3, PIN0 + choff + 2, down & 0xFF);
-	wiringPiI2CWriteReg8(3, PIN0 + choff + 3, (down >> 8) & 0xF);
+FLOAT inline fdiv(INT32 nomin, INT32 denom) {
+	return static_cast<FLOAT>(nomin) / static_cast<FLOAT>(denom);
 }
 
-inline void set(int ch, int val) {
-	if (val) setpwm(ch, 0, 4095);
-	else setpwm(ch, 0, 0);
+void ClearConsole() {
+#ifdef __linux__
+	system("clear");
+#else
+	system("cls");
+#endif
 }
 
-inline void sets(int servo, int a, int b, int c, int d) {
-	set(4 * servo + 0, a);
-	set(4 * servo + 1, b);
-	set(4 * servo + 2, c);
-	set(4 * servo + 3, d);
-}
+typedef enum { Horiz, Vertic, X = 0, Y = 1 } Motor;
+typedef enum { Left, Right } Direction;
 
-inline void rstep(int servo) {
-	switch (stepst[servo]) {
-	case 0:
-		sets(servo, 1, 0, 1, 0);
-		stepst[servo] = 1;
-		break;
-	case 1:
-		sets(servo, 0, 1, 1, 0);
-		stepst[servo] = 2;
-		break;
-	case 2:
-		sets(servo, 0, 1, 0, 1);
-		stepst[servo] = 3;
-		break;
-	case 3:
-		sets(servo, 1, 0, 0, 1);
-		stepst[servo] = 0;
-		break;
+namespace Pi {
+
+	inline void Init() {
+		wiringPiI2CSetup(0x41);
+		wiringPiI2CWriteReg8(3, 0x1, 0x4);
+		wiringPiI2CWriteReg8(3, 0x0, 0x1);
 	}
-}
 
-inline void lstep(int servo) {
-	switch (stepst[servo]) {
-	case 2:
-		sets(servo, 1, 0, 1, 0);
-		stepst[servo] = 1;
-		break;
-	case 3:
-		sets(servo, 0, 1, 1, 0);
-		stepst[servo] = 2;
-		break;
-	case 0:
-		sets(servo, 0, 1, 0, 1);
-		stepst[servo] = 3;
-		break;
-	case 1:
-		sets(servo, 1, 0, 0, 1);
-		stepst[servo] = 0;
-		break;
+	inline void SetPwm(int ch, int up, int down) {
+		int choff = 4 * ch;
+		wiringPiI2CWriteReg8(3, PIN0 + choff + 0, up & 0xFF);
+		wiringPiI2CWriteReg8(3, PIN0 + choff + 1, (up >> 8) & 0xF);
+		wiringPiI2CWriteReg8(3, PIN0 + choff + 2, down & 0xFF);
+		wiringPiI2CWriteReg8(3, PIN0 + choff + 3, (down >> 8) & 0xF);
 	}
-}
 
-void release() {
-	wiringPiI2CWriteReg8(3, 0xFA, 0);
-	wiringPiI2CWriteReg8(3, 0xFB, 0);
-	wiringPiI2CWriteReg8(3, 0xFC, 0);
-	wiringPiI2CWriteReg8(3, 0xFD, 0);
+	inline void Set(int ch, int val) {
+		if (val) SetPwm(ch, 0, 4095);
+		else SetPwm(ch, 0, 0);
+	}
+
+	inline void Set(int servo, int a, int b, int c, int d) {
+		Set(4 * servo + 0, a);
+		Set(4 * servo + 1, b);
+		Set(4 * servo + 2, c);
+		Set(4 * servo + 3, d);
+	}
+
+	inline void rStep(int servo) {
+		switch (stepst[servo]) {
+		case 0:
+			Set(servo, 1, 0, 1, 0);
+			stepst[servo] = 1;
+			break;
+		case 1:
+			Set(servo, 0, 1, 1, 0);
+			stepst[servo] = 2;
+			break;
+		case 2:
+			Set(servo, 0, 1, 0, 1);
+			stepst[servo] = 3;
+			break;
+		case 3:
+			Set(servo, 1, 0, 0, 1);
+			stepst[servo] = 0;
+			break;
+		}
+	}
+
+	inline void lStep(int servo) {
+		switch (stepst[servo]) {
+		case 2:
+			Set(servo, 1, 0, 1, 0);
+			stepst[servo] = 1;
+			break;
+		case 3:
+			Set(servo, 0, 1, 1, 0);
+			stepst[servo] = 2;
+			break;
+		case 0:
+			Set(servo, 0, 1, 0, 1);
+			stepst[servo] = 3;
+			break;
+		case 1:
+			Set(servo, 1, 0, 0, 1);
+			stepst[servo] = 0;
+			break;
+		}
+	}
+
+	void Release() {
+		wiringPiI2CWriteReg8(3, 0xFA, 0);
+		wiringPiI2CWriteReg8(3, 0xFB, 0);
+		wiringPiI2CWriteReg8(3, 0xFC, 0);
+		wiringPiI2CWriteReg8(3, 0xFD, 0);
+	}
+
 }
